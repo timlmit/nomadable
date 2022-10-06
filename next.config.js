c8fb67a63d9bae2f0022f1cd5b63e6e979aeca13
白水佑哉 <uzu@MacBook-Pro-7.local>
2022-10-06 09:17:59 +0700

    speed-test

--- next.config.js
+++ next.config.js
@@ -3,9 +3,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ["tripnote.sgp1.cdn.digitaloceanspaces.com"],
+  },
   headers: () => [
     {
-      source: "/speed-test-download/*",
+      source: "/speed-test-download/map.jpg",
       headers: [
         {
           key: "Cache-Control",
