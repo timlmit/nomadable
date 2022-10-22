2405ae5bc0b9fab7f08d0e29010fea97b5e90ec1
白水佑哉 <ys0520@gmail.com>
2022-10-22 15:27:02 +0700

    contents

--- components/article/IndexSection.tsx
+++ components/article/IndexSection.tsx
@@ -22,7 +22,7 @@ export const IndexSection: React.FC<Props> = ({
     <IndexSectionWrapper>
       <Label>
         <Square />
-        Index
+        Contents
       </Label>
       <IndexesWrapper>
         {placesWithData.map((place, index) => (
