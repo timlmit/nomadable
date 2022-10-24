fe8279db0a518dc941d1eaaf64cac9514b5c7776
白水佑哉 <ys0520@gmail.com>
2022-10-24 16:26:17 +0700

    work-loft

--- push-live.sh
+++ push-live.sh
@@ -6,7 +6,7 @@ mv robots.txt disabled-robots.txt
 git add .
 git commit -m "$1"
 git push live master
-ssh root@nomadable.net << EOF
+ssh -t root@nomadable.net << EOF
     cd /var/www/nomadable
     npm install
     npm run build
