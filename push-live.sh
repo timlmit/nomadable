fdea240ffbb80f169d89d141eb39ddef3657194d
白水佑哉 <ys0520@gmail.com>
2022-10-24 15:53:47 +0700

    add-work-loft

--- push-live.sh
+++ push-live.sh
@@ -3,4 +3,6 @@ npm run build
 git add .
 git commit -m "$1"
 git push live master
-ssh root@nomadable.net
+ssh root@nomadable.net << /Users/uzu/.ssh/id_rsa:
+command Siosio0520m
+/Users/uzu/.ssh/id_rsa
