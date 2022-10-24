e1bbb11cea354d9a7e4008ac69e621ac15261b37
白水佑哉 <ys0520@gmail.com>
2022-10-24 16:22:22 +0700

    work-loft

--- push-live.sh
+++ push-live.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
+eval `ssh-agent`
+ssh-add ~/.ssh/id_rsa
 mv robots.txt disabled-robots.txt
 # npm run build
 git add .
 git commit -m "$1"
 git push live master
 ssh root@nomadable.net
-expect "Enter passphrase for key '/root/.ssh/id_rsa':"
-send "Siosio0520m\r"
