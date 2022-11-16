3419b23caef4eb6f143528751f4e1877ec3e2ede
白水佑哉 <ys0520@gmail.com>
2022-11-16 11:55:51 +0700

    google-analytics

--- components/global/GlobalHead.tsx
+++ components/global/GlobalHead.tsx
@@ -26,6 +26,21 @@ export const GlobalHead = () => (
       }}
     />
 
+    {/* Google tag (gtag.js) */}
+    <Script
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TAG_MANAGER_ID}`}
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`        
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.GOOGLE_TAG_MANAGER_ID}');
+      `}
+    </Script>
+
     {/* Favicons */}
     <link
       rel="apple-touch-icon"
