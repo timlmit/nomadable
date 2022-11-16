fa3a2c2ac6aa91ac896e925eb493ef274b370a7a
白水佑哉 <ys0520@gmail.com>
2022-11-16 12:57:16 +0700

    google-analytics

--- components/global/GlobalHead.tsx
+++ components/global/GlobalHead.tsx
@@ -12,20 +12,6 @@ export const GlobalHead = () => (
     />
     {/* <script src="https://apis.google.com/js/platform.js" async defer></script> */}
 
-    <Script
-      id="gtm"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_ID}');
-      `,
-      }}
-    />
-
     {/* Google tag (gtag.js) */}
     <Script
       src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TAG_MANAGER_ID}`}
@@ -128,6 +114,16 @@ export const GlobalHead = () => (
             a.appendChild(r);
           })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
         `,
+      }}
+    />
+
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_ID}');`,
       }}
     />
 
