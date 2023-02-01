e5cd8e15fe665af953a98cb88fa1800f075593fc
白水佑哉 <ys0520@gmail.com>
2023-02-01 16:34:34 +0400

    dfs

--- push-live.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-eval `ssh-agent`
-ssh-add ~/.ssh/id_rsa
-mv robots.txt disabled-robots.txt
-# npm run build
-git add .
-git commit -m "$1"
-git push live master
-ssh -t root@nomadable.net 'bash -i -c "cd /var/www/nomadable.net && npm install && npm run build && pm2 restart nomadable"'
